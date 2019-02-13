dd07fc606f40e4c218b8fdca884f2c60c25f845d
“Martin <stuurmartipost@gmail.com>
2019-02-13 14:03:55 +0100

    event handler

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,38 @@
+// add eventhandler
+
 function submitComment (){
-  console.log('Hello!')
+  // console.log('Hello!')
+  let inputField = document.getElementById('name')
+  console.log(inputField)
+  let name = inputField.value
+  console.log(name)
+
+  // gather data txtarea
+  let txtInputField = document.getElementById('msg')
+  const msg = txtInputField.value
+  console.log(msg)
+
+  // create section element + store in variable  comment
+  const comment = document.createElement('section')
+
+  // create <h3> element + store in variable h3 
+
+  const h3 = document.createElement('h3')
+  // create <p> element + store in variable p
+  const p = document.createElement('p')
+  
+  // reassign innerhtml property of h3 variable to value of '${name} said:'
+  h3.innerHTML = `${name} said:`
+
+  // reassign innerhtml property of p variable to value of 'msg'
+  p.innerHTML = 'msg'
+
+  comment.classList.add("comment")
+
+  // use appendChild-method on comment to add your h3 and p variables to it
+
+  comment.appendChild(h3)
+  comment.appendChild(p)
+
+  console.log(comment)
 }
